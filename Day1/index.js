24a8159a7a4b1b8202220c739ed1c820ab4de6de
rvp15 <vedhagithub@gmail.com>
2024-06-26 12:49:04 -0400

    Class notes

---
+++ Day1/index.js
@@ -0,0 +1,9 @@
+
+
+const heading = 'Hello World!';
+const root = document.getElementById("root");
+root.textContent = heading
+
+// const reactHeading = React.createElement("h1" , {}, "Hello from react");
+// const appRoot = ReactDOM.createRoot(root);
+// appRoot.render(reactHeading);
