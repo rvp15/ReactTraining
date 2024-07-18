f856a87ffd41d45f4576098c8ab5324cfb00de36
rvp15 <vedhagithub@gmail.com>
2024-07-18 15:54:47 -0400

    updated

--- Day1/index.js
+++ Day1/index.js
@@ -1,9 +1,34 @@
 
 
-const heading = 'Hello World!';
-const root = document.getElementById("root");
-root.textContent = heading
+// const heading = 'Hello World!';
+// const root = document.getElementById("root");
+// root.textContent = heading
 
-// const reactHeading = React.createElement("h1" , {}, "Hello from react");
-// const appRoot = ReactDOM.createRoot(root);
-// appRoot.render(reactHeading);
+// Create a React element using React.createElement
+// const element = React.createElement(
+//     'h1',   // Type of the element
+//     { id: 'title', style: { color: 'blue' } },   // Props object
+//     'Hello, React!'   // Children (content)
+// );
+
+// nested element:
+const element2 = react.React.createElement(
+    'div',
+    {id:'container'},
+    React.createElement(
+        'p',
+        {id:'title',
+            style:{color: "blue"}
+        },'This is nested react',
+        ),
+        React.createElement(
+            'ul',
+            {id:'list-item'},
+            React.createElement('li',null, 'item1'),
+            React.createElement('li',null,'item2')
+        )
+)
+
+//ReactDOM.render(element, document.getElementById('root'));
+
+ReactDOM.render(element2, document.getElementById('root'));
